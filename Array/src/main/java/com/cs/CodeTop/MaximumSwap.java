97194b308d8ee5c8703a763a14832f443bbf8777
lishuaiyun <shuaiyun.li@bupt.edu.cn>
2021-10-14 14:46:06 +0800

    update

--- Array/src/main/java/com/cs/CodeTop/MaximumSwap.java
+++ Array/src/main/java/com/cs/CodeTop/MaximumSwap.java
@@ -8,9 +8,57 @@ package com.cs.CodeTop;
 * */
 public class MaximumSwap {
 
-    //和 下一个最大数字 有点像，都是找逆序对交换
-    public int maximumSwap(int num) {
 
-        return 0;
+    //暴力枚举
+    //和 下一个最大数字 有点像，都是找逆序对交换
+    //固定一位开始找最大逆序对交换 时间复杂度O(n^2) 空间复杂度O(1)
+    public int maximumSwap1(int num) {
+        String s = String.valueOf(num);
+        char[] nums = s.toCharArray();
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            int tmpIndex = i;
+            for (int j = i; j < nums.length; j++) {
+                if (nums[j] > nums[i]){
+                    tmpIndex = nums[j] >= nums[tmpIndex] ? j : tmpIndex;
+                }
+            }
+            if (tmpIndex == i){
+                continue;
+            }else {
+                swap(nums, i ,tmpIndex);
+                return Integer.valueOf(String.valueOf(nums));
+            }
+
+        }
+
+        return num;
+    }
+
+    private void swap(char[] nums, int i, int j) {
+        char tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+    //贪心算法
+    public int maximumSwap2(int num) {
+        String s = String.valueOf(num);
+        char[] nums = s.toCharArray();
+        //预处理
+        int[] indexData = new int[10];
+        for (int i = 0; i < nums.length; i++) {
+            indexData[nums[i] - '0'] = i;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 9; j > nums[i] - '0' ; j--) {
+                if (indexData[j] > i){
+                    swap(nums, i, indexData[j]);
+                    return Integer.valueOf(String.valueOf(nums));
+                }
+            }
+
+        }
+        return num;
     }
 }
